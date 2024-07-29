ff80d61a75c4825bbb311861b4b12ba71033bd12
Frederick <frederick.wennborg@gmail.com>
2024-07-29 11:21:06 +0200

    Added nav UI to sidebar

--- football-squad-builder-react/src/components/views/LineupCreator.tsx
+++ football-squad-builder-react/src/components/views/LineupCreator.tsx
@@ -10,23 +10,36 @@ function LineupCreator() {
                 </div>
 
                 <div className='lineup-creator'>
-
                     <div className="football-field" style={{ backgroundImage: `url(${footballField})` }}></div>
-
                     <aside className='sidebar'>
+                        <div className='sidebar-nav'>
+                        <select className='search-dropdown'>
+                                <option value="">Select League</option>
+                                <option value="league1">League 1</option>
+                                <option value="league2">League 2</option>
+                        </select>
+                        <select className='search-dropdown'>
+                                <option value="">Select Club</option>
+                                <option value="club1">Club 1</option>
+                                <option value="club2">Club 2</option>
+                                {/* Add more clubs as needed */}
+                            </select>
+                            <select className='search-dropdown'>
+                                <option value="">Select Player</option>
+                                <option value="player1">Player 1</option>
+                                <option value="player2">Player 2</option>
+                                {/* Add more players as needed */}
+                            </select>
+                        </div>
                         <div className='sidebar-section'>
                             <h2>Players</h2>
+                            <ol>
 
-
+                            </ol>
                         </div>
-
                     </aside>
-
-
                 </div>
-
             </section>
-
         </>
     )
 }
