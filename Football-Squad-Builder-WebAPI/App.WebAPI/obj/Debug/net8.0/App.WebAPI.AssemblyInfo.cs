6e7b64d2f951c500709a3731e72baae87205f423
Frederick <frederick.wennborg@gmail.com>
2024-08-15 13:27:29 +0200

    Merge from feature/feature-lineup-creator

--- Football-Squad-Builder-WebAPI/App.WebAPI/obj/Debug/net8.0/App.WebAPI.AssemblyInfo.cs
+++ Football-Squad-Builder-WebAPI/App.WebAPI/obj/Debug/net8.0/App.WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+606671d02a276bf27f045f3514e64b877429f410")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
